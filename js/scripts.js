a5cdeb4be3ec6436e35a2e23c323dfc154e6a9b6
Max Harlynking <mharlynk@andrew.cmu.edu>
2016-02-06 02:37:23 -0500

    initial commit, set up basic framework

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function($) { 
+    $("#owl").owlCarousel({
+        slideSpeed: 500,
+        autoPlay: true,
+        navigation: false,
+        pagination: false,
+        singleItem: true
+    });
+});
